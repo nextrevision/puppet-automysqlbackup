686288758157e59ddc9bde4a41c642834537f697
nextrevision <notarobot@nextrevision.net>
2013-02-13 10:14:03 -0500

    Added option to disable enforcement of permissions via cron and cleaned up formatting
    	modified:   Modulefile
    	modified:   manifests/init.pp
    	modified:   templates/automysqlbackup.conf.erb
    	modified:   templates/automysqlbackup.cron.erb

--- manifests/init.pp
+++ manifests/init.pp
@@ -34,6 +34,7 @@
 
 class automysqlbackup (
 	$cron_script = true,
+	$cron_set_permissions = true,
 	$mysql_dump_username = "",
 	$mysql_dump_password = "",
 	$mysql_dump_host = "",
@@ -80,7 +81,7 @@ class automysqlbackup (
 ) inherits automysqlbackup::params {
 
 	file { $etc_dir:
-    ensure  => directory,
+		ensure  => directory,
 		owner   => "root",
 		group   => "root",
 		mode    => 0750,
@@ -90,15 +91,15 @@ class automysqlbackup (
 		ensure  => file,
 		owner   => "root",
 		group   => "root",
-    mode    => 0660,
+		mode    => 0660,
 		source  => "puppet:///modules/automysqlbackup/automysqlbackup.conf",
 	}
-	
+
 	file { "$etc_dir/README":
 		ensure	=> file,
 		source	=> "puppet:///modules/automysqlbackup/README",
 	}
-	
+
 	file { "$etc_dir/LICENSE":
 		ensure	=> file,
 		source	=> "puppet:///modules/automysqlbackup/LICENSE",
@@ -111,13 +112,13 @@ class automysqlbackup (
 		mode    => 0755,
 		source  => "puppet:///modules/automysqlbackup/automysqlbackup",
 	}
-	
-	file {"$backup_dir":
-	  ensure  => directory,
-    owner   => "root",
-    group   => "root",
-    mode    => 0755,
-  }
+
+	file { "$backup_dir":
+		ensure  => directory,
+		owner   => "root",
+		group   => "root",
+		mode    => 0755,
+	}
 	if $cron_script {
 		file { "/etc/cron.daily/automysqlbackup":
 			ensure   => file,
@@ -130,51 +131,52 @@ class automysqlbackup (
 
 	# Creating a hash, really could probably avoid doing this, but wanted to try
 	$template_string_options = {
-    "mysql_dump_username" => $mysql_dump_username,
-    "mysql_dump_password" => $mysql_dump_password,
-    "mysql_dump_host" => $mysql_dump_host,
-    "mysql_dump_port" => $mysql_dump_port,
-    "backup_dir" => $backup_dir,
-    "multicore" => $multicore,
-    "multicore_threads" => $multicore_threads,
-    "do_monthly" => $do_monthly,
-    "do_weekly" => $do_weekly,
-    "rotation_daily" => $rotation_daily,
-    "rotation_weekly" => $rotation_weekly,
-    "rotation_monthly" => $rotation_monthly,
-    "mysql_dump_commcomp" => $mysql_dump_commcomp,
-    "mysql_dump_usessl" => $mysql_dump_usessl,
-    "mysql_dump_socket" => $mysql_dump_socket,
-    "mysql_dump_max_allowed_packet" => $mysql_dump_max_allowed_packet,
-    "mysql_dump_buffer_size" => $mysql_dump_buffer_size,
-    "mysql_dump_single_transaction" => $mysql_dump_single_transaction,
-    "mysql_dump_master_data" => $mysql_dump_master_data,
-    "mysql_dump_full_schema" => $mysql_dump_full_schema,
-    "mysql_dump_dbstatus" => $mysql_dump_dbstatus,
-    "mysql_dump_create_database" => $mysql_dump_create_database,
-    "mysql_dump_use_separate_dirs" => $mysql_dump_use_separate_dirs,
-    "mysql_dump_compression" => $mysql_dump_compression,
-    "mysql_dump_latest" => $mysql_dump_latest,
-    "mysql_dump_latest_clean_filenames" => $mysql_dump_latest_clean_filenames,
-    "mysql_dump_differential" => $mysql_dump_differential,
-    "mailcontent" => $mailcontent,
-    "mail_maxattsize" => $mail_maxattsize,
-    "mail_splitandtar" => $mail_splitandtar,
-    "mail_use_uuencoded_attachments" => $mail_use_uuencoded_attachments,
-    "mail_address" => $mail_address,
-    "encrypt" => $encrypt,
-    "encrypt_password" => $encrypt_password,
-    "prebackup" => $prebackup,
-    "postbackup" => $postbackup,
-    "umask" => $umask,
-    "dryrun" => $dryrun
+		"mysql_dump_username" => $mysql_dump_username,
+		"mysql_dump_password" => $mysql_dump_password,
+		"mysql_dump_host" => $mysql_dump_host,
+		"mysql_dump_port" => $mysql_dump_port,
+		"backup_dir" => $backup_dir,
+		"multicore" => $multicore,
+		"multicore_threads" => $multicore_threads,
+		"do_monthly" => $do_monthly,
+		"do_weekly" => $do_weekly,
+		"rotation_daily" => $rotation_daily,
+		"rotation_weekly" => $rotation_weekly,
+		"rotation_monthly" => $rotation_monthly,
+		"mysql_dump_commcomp" => $mysql_dump_commcomp,
+		"mysql_dump_usessl" => $mysql_dump_usessl,
+		"mysql_dump_socket" => $mysql_dump_socket,
+		"mysql_dump_max_allowed_packet" => $mysql_dump_max_allowed_packet,
+		"mysql_dump_buffer_size" => $mysql_dump_buffer_size,
+		"mysql_dump_single_transaction" => $mysql_dump_single_transaction,
+		"mysql_dump_master_data" => $mysql_dump_master_data,
+		"mysql_dump_full_schema" => $mysql_dump_full_schema,
+		"mysql_dump_dbstatus" => $mysql_dump_dbstatus,
+		"mysql_dump_create_database" => $mysql_dump_create_database,
+		"mysql_dump_use_separate_dirs" => $mysql_dump_use_separate_dirs,
+		"mysql_dump_compression" => $mysql_dump_compression,
+		"mysql_dump_latest" => $mysql_dump_latest,
+		"mysql_dump_latest_clean_filenames" => $mysql_dump_latest_clean_filenames,
+		"mysql_dump_differential" => $mysql_dump_differential,
+		"mailcontent" => $mailcontent,
+		"mail_maxattsize" => $mail_maxattsize,
+		"mail_splitandtar" => $mail_splitandtar,
+		"mail_use_uuencoded_attachments" => $mail_use_uuencoded_attachments,
+		"mail_address" => $mail_address,
+		"encrypt" => $encrypt,
+		"encrypt_password" => $encrypt_password,
+		"prebackup" => $prebackup,
+		"postbackup" => $postbackup,
+		"umask" => $umask,
+		"dryrun" => $dryrun
 	}
+
 	$template_array_options = {
 		"db_names" => $db_names,
-    "db_month_names" => $db_month_names,
-    "db_exclude" => $db_exclude,
-    "table_exclude" => $table_exclude,
-    "backup_local_files" => $backup_local_files,
+		"db_month_names" => $db_month_names,
+		"db_exclude" => $db_exclude,
+		"table_exclude" => $table_exclude,
+		"backup_local_files" => $backup_local_files,
 	}
 
 	# last but not least, create the automysqlbackup.conf
